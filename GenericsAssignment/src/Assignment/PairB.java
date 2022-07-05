c58a34369789021556031802875eeb182119005c
vijavvaj <vijavvaj@LIN66007448.corp.capgemini.com>
2022-07-05 13:39:03 +0530

    Generics Assignment

---
+++ GenericsAssignment/src/Assignment/PairB.java
@@ -0,0 +1,17 @@
+package Assignment;
+import java.util.Date;
+import java.util.HashMap;
+
+
+public class PairB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<String,Date> hm = new HashMap<String,Date>();
+		//adding key-value pair
+		hm.put("today is", new java.util.Date());
+		System.out.println(hm);
+
+	}
+
+}
---
+++ JavaAssignments/src/oopsAssignment/Shapes.java
@@ -0,0 +1,10 @@
+package oopsAssignment;
+
+public class Shapes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
