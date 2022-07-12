8dac9fdd95c9397513ee74826a3d43bf65f6c2d9
vijavvaj <vijavvaj@LIN66007448.corp.capgemini.com>
2022-07-12 11:37:17 +0530

    commit

---
+++ StreamApi/src/StreamAPI3/NewsId.java
@@ -0,0 +1,32 @@
+package StreamAPI3;
+
+public class NewsId {
+	private int newsId;
+    private String postedByUser;
+    private String commentByUser;
+    private String comment;
+
+    public void NewsID(int newsId, String postedByUser, String commentByUser, String comment) {
+        this.newsId = newsId;
+        this.postedByUser = postedByUser;
+        this.commentByUser = commentByUser;
+        this.comment = comment;
+    }
+
+    public int getNewsId() {
+        return newsId;
+    }
+
+    public String getPostedByUser() {
+        return postedByUser;
+    }
+
+    public String getCommentByUser() {
+        return commentByUser;
+    }
+
+    public String getComment() {
+        return comment;
+
+}
+}
